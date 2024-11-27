fa6f056a2a453f252ddf7b61fdeb3664910aa2d5
Daksh-Singh5 <dakshsingh6612@gmail.com>
2024-11-27 18:09:45 +0530

    hello

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ function add(x,y)
     document.getElementById("body1").style.backgroundColor="cyan";
 }
 
+function calculate() {
+    var getvalue = document.getElementById("number1").value;
+    var getvalue2 = document.getElementById("number2").value;
+
+    var result = Number(getvalue) + Number(getvalue2);
+
+    document.write("<br>"+"Your answer is: " + result);
+}
